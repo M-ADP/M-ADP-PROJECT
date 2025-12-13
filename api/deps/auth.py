b59ad9c04af82cf85df1cb717d8bfbec4a39727c
pdh0128 <pdh0128a@gmail.com>
2025-12-14 00:07:59 +0900

    feat(deps): 유저 정보 파싱 deps 함수 저으이

---
+++ api/deps/auth.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from fastapi import Header
+
+
+@dataclass
+class UserInfo:
+    user_id: str
+    role: str
+
+
+async def get_user_info(
+    user_id: str = Header(..., alias="user-id", description="사용자 식별자"),
+    role: str = Header(..., alias="role", description="사용자 역할"),
+) -> UserInfo:
+    return UserInfo(user_id=user_id, role=role)
