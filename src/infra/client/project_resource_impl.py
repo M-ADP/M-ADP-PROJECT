c03f0d7318a5c38ea76975e52b147d0256e5a1d4
pdh0128 <pdh0128a@gmail.com>
2026-01-17 18:52:19 +0900

    feat :: project resource mock client 정의

---
+++ src/infra/client/project_resource_impl.py
@@ -0,0 +1,45 @@
+from src.common.client.project_resource import ProjectResourceClient
+
+
+class MockProjectResourceClient(ProjectResourceClient):
+    """프로젝트 리소스 접근 Mock 클라이언트"""
+
+    async def create(self) -> None:
+        print("namespace 생성")
+        return None
+
+    async def delete(self) -> None:
+        print("namespace 삭제")
+        return None
+
+    async def open_port(self) -> None:
+        print("gateway 자원 생성")
+        return None
+
+    async def close_port(self) -> None:
+        print("gateway 자원 삭제")
+        return None
+
+    async def update_port(self) -> None:
+        print("gateway 자원 수정")
+        return None
+
+    async def create_dns(self) -> None:
+        print("ExternalDNS 자원 생성")
+        return None
+
+    async def delete_dns(self) -> None:
+        print("ExternalDNS 자원 삭제")
+        return None
+
+    async def update_dns(self) -> None:
+        print("ExternalDNS 자원 수정")
+        return None
+
+    async def mapping_dns_and_port(self) -> None:
+        print("ExternalDNS 자원과 gateway 자원 매핑")
+        return None
+
+    async def allocate(self) -> None:
+        print("Resource Quotar 자원 생성")
+        return None
