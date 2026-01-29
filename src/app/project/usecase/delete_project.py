39094e75e34cf5c2aea6baebda2c24cd2ce4e110
jaeminjo732 <jaeminjo732@gmail.com>
2026-01-29 20:30:23 +0900

    fix(project): 사용자 비밀번호를 user client로 검증

--- src/app/project/usecase/delete_project.py
+++ src/app/project/usecase/delete_project.py
@@ -11,11 +11,9 @@ class DeleteProjectUseCase(BaseUseCase):
         request: ProjectDelete,
         user_id: str,
     ) -> Project:
-        # is_valid = await self.project_resource_client.verify_password(
-        #     user_id, request.password
-        # )
-        # if not is_valid:
-        #     raise InvalidPassword()
+        is_valid = await self.user_client.verify_password(user_id, request.password)
+        if not is_valid:
+            raise InvalidPassword()
 
         project = await self.uow.project.get_by_id(project_id)
         if project is None:
