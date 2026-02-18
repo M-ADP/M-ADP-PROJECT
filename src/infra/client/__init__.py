d3a2badcddf6b63a6584757b46b05e518b4e1628
jaeminjo732 <jaeminjo732@gmail.com>
2026-02-18 19:42:00 +0900

    refactor(client): 사용자 클라이언트 export 정리

--- src/infra/client/__init__.py
+++ src/infra/client/__init__.py
@@ -1,11 +1,11 @@
 from src.infra.client.project_resource_impl import ProjectResourceClientImpl
 from src.infra.client.deployment_summary_impl import MockDeploymentSummaryClient
 from src.infra.client.deployment_impl import MockDeploymentClient
-from src.infra.client.user_impl import MockUserClient
+from src.infra.client.user_impl import UserClientImpl
 
 __all__ = [
     "ProjectResourceClientImpl",
     "MockDeploymentSummaryClient",
     "MockDeploymentClient",
-    "MockUserClient",
+    "UserClientImpl",
 ]
