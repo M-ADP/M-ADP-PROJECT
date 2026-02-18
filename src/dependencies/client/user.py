fea1178f9518617a400a51d169904d4236f1f34d
jaeminjo732 <jaeminjo732@gmail.com>
2026-02-18 19:42:00 +0900

    refactor(user): 사용자 클라이언트 의존성 구현체 교체

--- src/dependencies/client/user.py
+++ src/dependencies/client/user.py
@@ -1,6 +1,7 @@
 from src.core.client.user import UserClient
-from src.infra.client.user_impl import MockUserClient
+from src.common.config.user_server import get_user_server_config
+from src.infra.client.user_impl import UserClientImpl
 
 
 def get_user_client() -> UserClient:
-    return MockUserClient()
+    return UserClientImpl(user_server_config=get_user_server_config())
