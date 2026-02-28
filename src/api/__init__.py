aa2894103b85aa2c7aad5bbe767346c4cbc1f231
jaeminjo732 <jaeminjo732@gmail.com>
2026-02-28 22:53:13 +0900

    refactor(api): create_app에서 미들웨어 등록 호출 제거

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,7 +2,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.api.middlewares import register_middlewares
 from src.api.routers import api_router
 from src.common.config.settings import get_settings
 from src.core.db import create_all_tables
@@ -22,7 +21,6 @@ def create_app() -> FastAPI:
         version=settings.app_version,
         lifespan=lifespan,
     )
-    register_middlewares(application, settings)
     application.include_router(api_router)
     register_exception_handlers(application)
 
