268261b750557883c93906bc19bf6aac64712b86
jaeminjo732 <jaeminjo732@gmail.com>
2026-02-28 22:53:15 +0900

    refactor(infra): 클라이언트 export를 MockApplicationClient로 교체

--- src/infra/client/__init__.py
+++ src/infra/client/__init__.py
@@ -1,11 +1,11 @@
 from src.infra.client.project_resource_impl import ProjectResourceClientImpl
 from src.infra.client.deployment_summary_impl import MockDeploymentSummaryClient
-from src.infra.client.deployment_impl import MockDeploymentClient
+from src.infra.client.application_impl import MockApplicationClient
 from src.infra.client.user_impl import UserClientImpl
 
 __all__ = [
     "ProjectResourceClientImpl",
     "MockDeploymentSummaryClient",
-    "MockDeploymentClient",
+    "MockApplicationClient",
     "UserClientImpl",
 ]
