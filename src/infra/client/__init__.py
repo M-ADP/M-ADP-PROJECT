b35cc0926b9a3f68b6df7ac97b7b8a6266916fa6
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-02 00:53:53 +0900

    refactor(infra): infra client export를 ApplicationClientImpl 기준으로 정리

--- src/infra/client/__init__.py
+++ src/infra/client/__init__.py
@@ -1,11 +1,10 @@
 from src.infra.client.project_resource_impl import ProjectResourceClientImpl
-from src.infra.client.deployment_summary_impl import MockDeploymentSummaryClient
-from src.infra.client.application_impl import MockApplicationClient
+from src.infra.client.application_impl import ApplicationClientImpl, MockApplicationClient
 from src.infra.client.user_impl import UserClientImpl
 
 __all__ = [
     "ProjectResourceClientImpl",
-    "MockDeploymentSummaryClient",
+    "ApplicationClientImpl",
     "MockApplicationClient",
     "UserClientImpl",
 ]
