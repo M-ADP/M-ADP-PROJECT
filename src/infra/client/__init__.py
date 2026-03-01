4ba0c397364510faf512a7789d693293ce89a095
jaeminjo732 <jaeminjo732@gmail.com>
2026-03-02 03:51:08 +0900

    refactor(infra): infra client export에서 MockApplicationClient 제거

--- src/infra/client/__init__.py
+++ src/infra/client/__init__.py
@@ -1,10 +1,9 @@
 from src.infra.client.project_resource_impl import ProjectResourceClientImpl
-from src.infra.client.application_impl import ApplicationClientImpl, MockApplicationClient
+from src.infra.client.application_impl import ApplicationClientImpl
 from src.infra.client.user_impl import UserClientImpl
 
 __all__ = [
     "ProjectResourceClientImpl",
     "ApplicationClientImpl",
-    "MockApplicationClient",
     "UserClientImpl",
 ]
